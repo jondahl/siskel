118b4695a6d2daef4b49799b3e57c98289b6add0
Jon Dahl <jon@tumblon.com>
2009-07-18 15:16:53 -0500

    implement nested values (nested by Video or Audio streams)

--- lib/siskel.rb
+++ lib/siskel.rb
@@ -1,3 +1,4 @@
+require 'ruby-debug'
 class Siskel
 
   #   results = Siskel.review(:file => 'path/to/myfile.mov')
@@ -7,22 +8,34 @@ class Siskel
   class << self
     def review(options = {})
       raise ArgumentError, "Must supply either an input file or a pregenerated response" if options[:raw_response].nil? and options[:file].nil?
-    
+      raise ArgumentError, "File not found: #{options[:file]}" if options[:file] and !FileTest.exist?(options[:file])
       parse_results(options[:raw_response] || `mediainfo #{options[:file]}`)
     end
   
     def parse_results(mediainfo_results)
-      
       mediainfo_results.split("\n").inject({}) do |siskel_results, row|
-        key, value = parse_row(row)
-        siskel_results[key] = value if key and value
+        result = parse_row(row)
+        if result.is_a?(Symbol)
+          @prefix = result
+          siskel_results[@prefix] ||= {}
+        elsif result.is_a?(Hash)
+          if @prefix.nil?
+            siskel_results[result.keys.first] = result.values.first
+          else
+            siskel_results[@prefix][result.keys.first] = result.values.first
+          end
+        end
         siskel_results
       end
     end
     
     def parse_row(row)
       results = /(.*) : (.*)/.match(row)
-      return make_key(results[1]), results[2].strip if results
+      if results
+        return { make_key(results[1]) => results[2].strip }
+      elsif row =~ /\w+/ && row !=~ /General/i
+        return row.strip.downcase.to_sym
+      end
     end
     
     def make_key(str)
--- test/siskel_test.rb
+++ test/siskel_test.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'test/unit'
 require 'shoulda'
 require 'fixtures/test_mediainfo_results'
-require '../lib/siskel'
+require 'lib/siskel'
 
 class SiskelTest < Test::Unit::TestCase
   
@@ -17,20 +17,32 @@ class SiskelTest < Test::Unit::TestCase
       assert_equal Hash, results.class
     end
     
+    should "raise an exception if neither file nor raw results are provided" do
+      assert_raises ArgumentError do
+        results = Siskel.review
+      end
+    end
+    
+    should "raise an exception if file not found" do
+      assert_raises ArgumentError do
+        results = Siskel.review(:file => 'fixtures/not_there.mp4')
+      end
+    end
+    
     context "results" do
       setup do
         @results = Siskel.review(:raw_response => TestMediainfoResults.triple_figures)
       end
       
       should "have general attributes" do
-        assert @results[:format]
-        puts @results.inspect
+        assert @results[:general]
         assert @results[:video]
-        assert @results[:audio]
+        assert @results[:audio][:format]
       end
     end
     
     context "compatibility" do
+      # TODO: add suite of compatibility tests
       # should_inspect_media :file => 'file'
       # should_inspect_media :response => 'response' 
     end
